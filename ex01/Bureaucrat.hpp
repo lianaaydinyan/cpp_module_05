8dc4559b9d514e411a21862af2c5e3d95315b397
lianaaydinyan <lianaaydinyan>
2024-09-01 13:21:53 +0400

    finish

--- ex01/Bureaucrat.hpp
+++ ex01/Bureaucrat.hpp
@@ -35,6 +35,6 @@ class Bureaucrat
 
 };
 
-std::ostream& operator<<(std::ostream o, const Bureaucrat obj);
+std::ostream& operator<<(std::ostream o, const Bureaucrat& obj);
 
 #endif // Bureaucrat_hpp
